670109464e759f1fa50c8d066dcf6689b82c9bd2
Doğan Çeçen <sepeth@hey.com>
2026-06-14 19:20:27 +0200

    Add astrocommunity.pack.cpp for debugging

--- lua/community.lua
+++ lua/community.lua
@@ -7,5 +7,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.pack.cpp" },
   -- import/override with your plugins folder
 }
